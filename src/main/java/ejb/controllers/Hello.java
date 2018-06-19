ab617c75b8b60bae59addd1c987aa87b0edda480
Irina <irishkinson@list.ru>
2018-06-19 16:22:00 +0300

    Hello world EJB

--- src/main/java/ejb/controllers/Hello.java
+++ src/main/java/ejb/controllers/Hello.java
@@ -1,11 +1,13 @@
 package ejb.controllers;
 
 
+import ejb.beans.Consumer;
 import ejb.beans.HelloEjb;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
@@ -15,15 +17,22 @@ public class Hello implements Serializable {
 
     @EJB
     private HelloEjb helloEjb;
+    @EJB
+    private Consumer consumer;
 
     private String message;
 
+    private String messageQueu;
+
     public void setName(String name) {
         message = helloEjb.sayHello(name);
+        consumer.receive();
     }
 
     public String getMessage() {
         return message;
     }
+
+
 
 }
